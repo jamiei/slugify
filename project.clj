afda2a16f8db9f9bae1a1942d78596b91f8fa2a9
Jamie Ingilby <jamie@op-i.net>
2013-06-09 22:51:25 +0100

    Removed binaries, updated namespace to de-dup and updated to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
-(defproject slugify "0.0.1"
+(defproject com.jamiei.slugify "0.0.2"
   :description "Simple slug generator. Transforms strings into URL slugs, removing non-alphanumeric characters and spaces."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                          [midje "1.5.1"]]
+  :min-lein-version "2.0.0")
